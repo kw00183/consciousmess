aaae91d161f96586bd905f41123cdb4975223901
kw00183 <kw00183@cs6242-vm02.uwg.westga.edu>
2022-02-27 00:18:57 -0500

    comment out parts of the data in teh view not needed; move if statement on name link to helper method name_link and update the view

--- app/helpers/ripples_helper.rb
+++ app/helpers/ripples_helper.rb
@@ -141,4 +141,13 @@ module RipplesHelper
       return link_to string, url
     end
   end
+
+  # handles the name url
+  def name_link(string,url)
+    if url == ''
+      return string
+    else
+      return link_to string, url, :target => "_blank"
+    end
+  end
 end
